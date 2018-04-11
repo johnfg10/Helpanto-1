3b98316601135b3146a8b6ab9e79bec348d408bb
johnfg10 <johnfg2610@gmail.com>
2018-04-11 17:02:42 +0100

    added support for swearword.txt

--- scripts/swearfilter.kts
+++ scripts/swearfilter.kts
@@ -1,11 +1,21 @@
+import java.io.File
+import java.io.FileReader
+
 //list of swear words thats filtered
-val bannedWords = listOf(
+var bannedWords = listOf(
     "fuck", "shit", "bastard", "cunt", "twat", "wanker"
 )
 
 fun isAllowed(message: String) : Boolean {
+    val file = File("");
+    println(file.absoluteFile)
+    if(File("${file.absoluteFile}/scripts/swearword.txt").exists()){
+        val texts = File("${file.absoluteFile}/scripts/swearword.txt").readLines()
+        bannedWords = texts
+    }
+
     for(bannedWord in bannedWords){
-        if(message.contains(Regex(bannedWord))){
+        if(message.contains(bannedWord)){
             return false
         }
     }
