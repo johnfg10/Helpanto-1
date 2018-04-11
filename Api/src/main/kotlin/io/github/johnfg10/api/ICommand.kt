28b7b538c0d732ecb0d4d124c85bc4a1ca7d7473
johnfg10 <johnfg2610@gmail.com>
2018-04-11 16:40:51 +0100

    added javadocs

--- Api/src/main/kotlin/io/github/johnfg10/api/ICommand.kt
+++ Api/src/main/kotlin/io/github/johnfg10/api/ICommand.kt
@@ -2,8 +2,21 @@ package io.github.johnfg10.api
 
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent
 
+/**
+ * by implementing the methods in this class a script will be considered as a "command" and will be treated as such internally
+ */
 interface ICommand{
+    /**
+     * called when a "command" is called
+     *
+     * @param event the event of a matching message
+     * @param args a list of args excluding the matching argument and prefix
+     */
     fun execCommand(event: MessageReceivedEvent, args: List<String>)
 
+    /**
+     * the name used by the internal system to recognise this command
+     * @return the name of the command which is matched to call @see execCommand
+     */
     fun getCommandName() : String
 }
--- Api/src/main/kotlin/io/github/johnfg10/api/IFilter.kt
+++ Api/src/main/kotlin/io/github/johnfg10/api/IFilter.kt
@@ -3,6 +3,14 @@ package io.github.johnfg10.api
 import sx.blah.discord.handle.impl.obj.Message
 import sx.blah.discord.handle.obj.IMessage
 
+/**
+ * Repusents a filter in the internal system
+ */
 interface IFilter {
+    /**
+     * is called on every message
+     * @param message the message to filter
+     * @return returns true if the message is permitted and false otherwise
+     */
     fun isAllowed(message: String) : Boolean
 }
